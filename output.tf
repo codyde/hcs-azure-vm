bbaad94d6091c15082bc4b7b73317568383ca1bb
Cody De Arkland <cody@hashicorp.com>
2020-03-30 17:59:54 -0700

    removing VMs

--- output.tf
+++ output.tf
@@ -1,9 +1,11 @@
+/*
 output "public_ip_address1" {
   description = "The actual ip address allocated for the resource."
-  value       = azurerm_public_ip.this1.*.ip_address
+  value       = azurerm_public_ip.vm1.*.ip_address
 }
 
 output "public_ip_address2" {
   description = "The actual ip address allocated for the resource."
-  value       = azurerm_public_ip.this2.*.ip_address
+  value       = azurerm_public_ip.vm2.*.ip_address
 }
+*/
