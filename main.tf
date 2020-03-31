b5eca8dbd5f02fb3d2a4ea5b6ba0090f625f5716
Cody De Arkland <cody@hashicorp.com>
2020-03-30 17:25:16 -0700

    updating parameters and security group*

--- main.tf
+++ main.tf
@@ -30,6 +30,25 @@ resource "azurerm_network_interface" "example" {
   }
 }
 
+resource "azurerm_network_security_group" "netallow" {
+  name                = "allow-networks"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+
+  security_rule {
+    name                       = "networksAllow"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "10.0.0.0/16"
+    destination_address_prefix = "*"
+  }
+
+}
+
 resource "azurerm_public_ip" "this" {
   name                = "hcsclient04ip"
   location            = "West US 2"
@@ -49,7 +68,7 @@ resource "azurerm_linux_virtual_machine" "hcsclient04" {
 
   admin_ssh_key {
     username   = "codyhc"
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDZWetJdpOowRmu5PeaYbKK9WPKiC3w5cB1PpVGZZwKuSczAUYBmJTYBnv5Hs4c5Iz0j/87fqqEoFuwPv1E08r69EqbgmTdREBxTm+7lzCTsPCMo4G4DZFWwUwd+ZarA7j22f0Mik2iUOMWml3BUemvymYXwO9unwwkFRvCaWtXNtIEVimLms1ILCMld4MolXgyIwpfR3uXDCCcPjIHnAXOD0ziuZQ7x9WG6I9d/kO1JiYaa3sDWNkjQiP7qcp8OjMV6sTuE+1xljDDfgf8Ze8vJNdLauFGDX6sm8Cw1E49Qhg5Jc0fzjJVkLV8zkFOWF0Vp777m5oE2sjOVCQjLKx/I6529v53C5Wm5e8bgmPkjaPiJUTvH27NEuJqFI1TDPEapm/s5pBFxVlTndYILojWxuozrFusaIBC4z6DcV9dVpHFBb6zIQDlWH8W0jlsoKFKE4JS8qmI2pWWK/e4za3SGZOH+KZqetXK5i7u8633xGZt0pIcjUP4rozzLrlO8QIGVtUITnRzdrkTu+gHPxTkRoKgpnO5xIAajnWQ1M8VUdymCGCiTast9WTFkHg6GYlcBV8FK+2ALDbKKbESZn/qcLdFDE5BKtI3eHomuENvTQr+q2MjsczaolomLKhgUcMRMmIxOVaOM0czAuMd1tiVIBqOu3U3NhywOgJITX/saw=="
+    public_key = var.sshkey
   }
 
   os_disk {
@@ -57,5 +76,5 @@ resource "azurerm_linux_virtual_machine" "hcsclient04" {
     storage_account_type = "Standard_LRS"
   }
 
-  source_image_id = "/subscriptions/2835f892-3393-4988-bf4b-c169a8b22f1e/resourceGroups/cdearkland-hcs-rg/providers/Microsoft.Compute/images/consul-hcs-cde"
+  source_image_id = var.image
 } 
--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-variables "sshkey" {
+variable "sshkey" {
     description = "Public SSH Key for remote sessions"
 }
 
-variables "image" {
+variable "image" {
     description = "Consul image for Azure deployment"
 }
