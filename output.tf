57a46cced6c05be6ad69b27f6746e78f29d9344e
Cody De Arkland <cody@hashicorp.com>
2020-03-30 17:40:35 -0700

    Correcting output

--- output.tf
+++ output.tf
@@ -1,4 +1,9 @@
-output "public_ip_address" {
+output "public_ip_address1" {
   description = "The actual ip address allocated for the resource."
-  value       = azurerm_public_ip.this.*.ip_address
+  value       = azurerm_public_ip.this1.*.ip_address
+}
+
+output "public_ip_address2" {
+  description = "The actual ip address allocated for the resource."
+  value       = azurerm_public_ip.this2.*.ip_address
 }
