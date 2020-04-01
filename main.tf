1e727b8c6651f4a33c1c90b13c302e29624174a5
Cody De Arkland <cody@hashicorp.com>
2020-03-31 23:56:29 -0700

    Rebuild

--- main.tf
+++ main.tf
@@ -121,3 +121,132 @@ resource "azurerm_linux_virtual_machine" "hcsclient05" {
 
   source_image_id = var.image
 } 
+
+resource "azurerm_public_ip" "nomad1" {
+  name                = "hcsnomad01ip"
+  location            = "West US 2"
+  resource_group_name = data.azurerm_resource_group.this.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nomad1" {
+  name                = "nomadext01"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nomad1.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "hcsnomad01" {
+  name                = "hcsnomad01"
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  size                = "Standard_B1ms"
+  admin_username      = "codyhc"
+  network_interface_ids = [
+    azurerm_network_interface.nomad1.id,
+  ]
+
+  admin_ssh_key {
+    username   = "codyhc"
+    public_key = var.sshkey
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id = var.nomadimage
+} 
+
+resource "azurerm_public_ip" "nomad2" {
+  name                = "hcsnomad02ip"
+  location            = "West US 2"
+  resource_group_name = data.azurerm_resource_group.this.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nomad2" {
+  name                = "nomadext2"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nomad2.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "hcsnomad02" {
+  name                = "hcsnomad02"
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  size                = "Standard_B1ms"
+  admin_username      = "codyhc"
+  network_interface_ids = [
+    azurerm_network_interface.nomad2.id,
+  ]
+
+  admin_ssh_key {
+    username   = "codyhc"
+    public_key = var.sshkey
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id = var.nomadimage
+} 
+
+resource "azurerm_public_ip" "nomad3" {
+  name                = "hcsnomad03ip"
+  location            = "West US 2"
+  resource_group_name = data.azurerm_resource_group.this.name
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nomad3" {
+  name                = "nomadext03"
+  location            = data.azurerm_resource_group.this.location
+  resource_group_name = data.azurerm_resource_group.this.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = data.azurerm_subnet.this.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.nomad3.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "hcsnomad03" {
+  name                = "hcsnomad03"
+  resource_group_name = data.azurerm_resource_group.this.name
+  location            = data.azurerm_resource_group.this.location
+  size                = "Standard_B1ms"
+  admin_username      = "codyhc"
+  network_interface_ids = [
+    azurerm_network_interface.nomad3.id,
+  ]
+
+  admin_ssh_key {
+    username   = "codyhc"
+    public_key = var.sshkey
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_id = var.nomadimage
+} 
